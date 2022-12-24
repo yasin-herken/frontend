0f5f486deb8876aca9e5754d1b7de18b2e950be7
yasinherken <yasin.herken123@gmail.com>
2022-12-25 01:12:06 +0300

    Request method is changed to the real one

--- src/Requests/RequestsMethods.js
+++ src/Requests/RequestsMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8081";
+export const BASE_URL = "https://expert-system.onrender.com";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
